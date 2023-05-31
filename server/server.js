d0346b81564b80adcb3a780193975b62f0fc5abb
Michal2232 <elgawi@g.jct.ac.il>
2023-05-31 23:29:11 +0300

    first step of server setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+// מקבל בקשה מהלקוח, שולח בקשה לשרת,השרת מבצע חיפוש במסד נתונים ומחזיר מידע ללקוח
 const express = require("express");
 const cors = require('cors');
 
 require("dotenv").config();
 
-const app = express(); // initialize the app
-app.use(cors());
+const app = express();  // initialize the app
+app.use(cors());        //App security
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
